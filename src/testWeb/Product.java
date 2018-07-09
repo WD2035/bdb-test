441abecee2f7d16084c70910eabef057165a11df
WD2035 <woidor@bancodebogota.com.co>
2018-07-09 11:41:32 -0500

    modify product classs

--- src/testWeb/Product.java
+++ src/testWeb/Product.java
@@ -2,6 +2,21 @@ package testWeb;
 
 public class Product {
 	
+	private Client client;
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public Bank getBank() {
+		return bank;
+	}
+	public void setBank(Bank bank) {
+		this.bank = bank;
+	}
+	private Bank bank;
+	
 	
 
 }
